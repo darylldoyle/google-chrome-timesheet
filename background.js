0a1af4875423be1bf8d46f621b0b1d8b4e5d5d8d
Daryll Doyle <daryll.doyle@10up.com>
2024-07-31 09:58:40 +0100

    Updates to get TW working and some different styling for pop-up

--- background.js
+++ background.js
@@ -27,47 +27,51 @@ const backgroundHandler = async function (message, sender, sendResponse) {
 		}
 	} );
 
-	// Parse harvest time entries
-	const resHarvest = await fetch(`https://api.harvestapp.com/v2/time_entries?from=${message.start}&to=${message.end}`, {
-		method: 'GET',
-		headers: {
-			'Harvest-Account-Id': message.harvestId,
-			'authorization': `Bearer ${message.harvestApiKey}`,
-		}
-	});
-	const responseHarvest = await resHarvest.json();
+	if ( message.harvestId && message.harvestApiKey ) {
+		// Parse harvest time entries
+		const resHarvest = await fetch(
+			`https://api.harvestapp.com/v2/time_entries?from=${message.start}&to=${message.end}`,
+			{
+				method: 'GET',
+				headers: {
+					'Harvest-Account-Id': message.harvestId,
+					'authorization': `Bearer ${message.harvestApiKey}`,
+				}
+			});
+		const responseHarvest = await resHarvest.json();
 
-	const harvestTimeEntries = responseHarvest['time_entries'];
-	harvestTimeEntries.forEach( function( el ) {
-		const key = `cpt${el.client.id}-${el.project.id}-${el.task.id}`;
+		const harvestTimeEntries = responseHarvest['time_entries'];
+		harvestTimeEntries.forEach(function(el) {
+			const key = `cpt${el.client.id}-${el.project.id}-${el.task.id}`;
 
-		if (timesheet[key] === undefined) {
-			timesheet[key] = {
-				'client': el.client.name,
-				'client_id': el.client.id,
-				'project_id': el.project.id,
-				'project': el.project.name,
-				'task_id': el.task.id,
-				'task': el.task.name,
-				'hours': parseFloat( el.rounded_hours )
-			};
-		} else {
-			timesheet[key].hours += parseFloat( el.rounded_hours );
-		}
-	} );
-
-	console.log(timesheet)
+			if (timesheet[key] === undefined) {
+				timesheet[key] = {
+					'client': el.client.name,
+					'client_id': el.client.id,
+					'project_id': el.project.id,
+					'project': el.project.name,
+					'task_id': el.task.id,
+					'task': el.task.name,
+					'hours': parseFloat(el.rounded_hours)
+				};
+			} else {
+				timesheet[key].hours += parseFloat(el.rounded_hours);
+			}
+		});
+	}
 
 	const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
 
-	chrome.tabs.sendMessage(
-		tab.id,
-		{ 
-			type: 'parseTimesheet', 
-			timeEntries: timesheet, 
-			startDate: message.start,
-		}
+	if (tab && 'id' in tab) {
+		chrome.tabs.sendMessage(
+			tab.id,
+			{
+				type: 'parseTimesheet',
+				timeEntries: timesheet,
+				startDate: message.start,
+			}
 		);
+	}
 };
 
 chrome.runtime.onMessage.addListener(backgroundHandler);
